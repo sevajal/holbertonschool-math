e940c664665bdbbeadebad316de8f7e65e1d4173
sevajal <sevajal@yahoo.com>
2022-03-17 16:28:41 -0500

    I created the header file

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <math.h> 
+
+void display_complex_number(complex c);
+complex conjugate(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg (double m, double arg, complex *c3);
+
+#endif
