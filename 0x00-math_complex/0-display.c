8ebb31de4d9b5a1cef1f281132a507fdcc8698f8
sevajal <sevajal@yahoo.com>
2022-03-17 22:13:38 -0500

    I created the code

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -13,7 +13,17 @@ void display_complex_number(complex c)
 	if (c.im == 0)
 		printf("%.0f\n", c.re);
 	else if (c.im > 0)
-		printf("%.0f + %.0fi\n", c.re, c.im);
+	{
+		if (c.im == 1)
+			printf("%.0f + i\n", c.re);
+		else
+			printf("%.0f + %.0fi\n", c.re, c.im);
+	}
 	else if (c.im < 0)
-		printf("%.0f - %.0fi\n", c.re, (c.im * - 1));
+	{
+		if (c.im == -1)
+			printf("%.0f - i\n", c.re);
+		else
+			printf("%.0f - %.0fi\n", c.re, (c.im * -1));
+	}
 }
