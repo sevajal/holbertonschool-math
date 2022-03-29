ee25c1405530d3c57135e350c9ca432ca44fd711
sevajal <sevajal@yahoo.com>
2022-03-29 11:03:23 -0500

    I changed the code for floats

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,20 +10,32 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-		printf("%.0f\n", c.re);
-	else if (c.im > 0)
+  char operation;
+  int intre, intim;
+  
+  intim = c.im;
+  intre = c.re;
+  
+    if (c.im < 0)
+    {
+      operation = '-';
+      c.im = c.im * -1;
+    }
+    else
+    {
+      operation = '+';
+      c.im = c.im;
+    }
+    
+    if (c.im == 0)
+        printf("%.0f\n", c.re);
+    if (c.im != 0)
 	{
-		if (c.im == 1)
-			printf("%.0f + i\n", c.re);
-		else
-			printf("%.0f + %.0fi\n", c.re, c.im);
-	}
-	else if (c.im < 0)
-	{
-		if (c.im == -1)
-			printf("%.0f - i\n", c.re);
-		else
-			printf("%.0f - %.0fi\n", c.re, (c.im * -1));
+    	if (c.im - intim || c.re - intre)
+        	printf("%.1f %c %.1fi\n", c.re, operation, c.im);
+      	else if (c.im == 1)
+        	printf("%.0f %c i\n", c.re, operation);
+      	else
+        	printf("%.0f %c %.0fi\n", c.re, operation, c.im);
 	}
 }
